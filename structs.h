41bbd8e537d7322f6f880a6e669504a20e51f43b
facundo <facubarrera80@gmail.com>
2021-11-28 20:47:50 -0300

    Creacion y codificacion de structs.h

---
+++ structs.h
@@ -0,0 +1,51 @@
+/**
+ * 
+ * Este archivo contiene todas las estructuras que se utilizaran durante la ejecucion
+ * del programa.
+ * 
+ */
+
+struct Fecha
+{
+    int dia, mes, anio;
+};
+
+struct Usuario
+{
+    char usuario[10];
+    char contrasena[10];
+    int tipo;
+};
+
+struct Profesional
+{
+    char usuario[10];
+    char nombre[60];
+    int id_profesional;
+    int dni;
+    char telefono[25];
+};
+
+struct Cliente
+{
+    int dni;
+    char nombre[60];
+    char domicilio[60];
+    char localidad[60];
+    Fecha fecha_nacimiento;
+    float peso;
+    char telefono[25];
+};
+
+struct Turno
+{
+    int id_profesional;
+    Fecha fecha;
+    int dni_cliente;
+    char informe[380];
+};
+
+struct Sesion
+{
+    Usuario usuario;
+};
