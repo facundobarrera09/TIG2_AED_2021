0fac6fd542aef695a8ddbe9aa3ab43cd82ca01c8
facundo <facubarrera80@gmail.com>
2021-11-28 21:11:19 -0300

    Creacion y primera codificación (main) de CentroEstetica.cpp

---
+++ CentroEstetica.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "structs.h"
+
+int inicio_de_sesion(Usuario &usuario);
+
+main()
+{
+    Usuario usuario;
+    int estado;
+
+    //mostrar_logo();
+
+    estado = inicio_de_sesion(usuario);
+}
+
+int inicio_de_sesion(Usuario &usuario)
+{
+
+}
