b142d8205422f1736df1dd408871daf0456a6c81
facundo <facubarrera80@gmail.com>
2022-01-19 00:11:55 -0300

    Correcion de errores

--- test.cpp
+++ test.cpp
@@ -75,90 +75,53 @@ void test0()
     Error *errores;
     int cantidad = 3, tipo = 1, estado;
 
-    strcpy(usuarios[0].usuario, "facundo");
-    strcpy(usuarios[1].usuario, "lucia");
-    strcpy(usuarios[2].usuario, "carla");
+    if (leer_usuarios(usuarios, cantidad) == 0)
+    {
+        estado = inicio_de_sesion(usuario, tipo, usuarios, cantidad, errores);
 
-    strcpy(usuarios[0].contrasena, "facundo");
-    strcpy(usuarios[1].contrasena, "lucia");
-    strcpy(usuarios[2].contrasena, "carla");
-
-    usuarios[0].tipo = 1;
-    usuarios[1].tipo = 1;
-    usuarios[2].tipo = 2;
-
-    estado = inicio_de_sesion(usuario, tipo, usuarios, cantidad, errores);
-
-    printf("estado=%d\n", estado);
-    mostrar_errores(errores);
+        printf("estado=%d\n", estado);
+        mostrar_errores(errores);
+    }
+    else
+        printf("ERROR - No existen usuarios o no se puede leer el archivo\n");
 }
 
 void test1()
 {
-    Error e, *e1 = NULL, *e2 = NULL, *e3 = NULL;
+    Error *errores = NULL;
     Usuario usuarios[MAX_USUARIOS];
     int cantidad = 0;
 
-    leer_usuarios(usuarios, cantidad);
-
-    int status1 = crear_usuario(usuarios, cantidad, e1);
-    int status2 = crear_usuario(usuarios, cantidad, e2);
-    int status3 = crear_usuario(usuarios, cantidad, COD_ASIST, e3);
-
-/*
-faC12Bar
-NX
-123ab
-NX
-ADMIN
-OK
-Fac123barerr
-NX
-1234.aB
-NX
-PROF
-OK
-lUC1aBarr
-NX
-aM3h2dI
-NX
-OK
-
-*/
-
-    printf("ESTADOS:\n\tstatus1=%d\n\tstatus2=%d\n\tstatus3=%d\n", status1, status2, status3);
-
-    printf("\nUsuario 1:\n");
-    if (e1 != NULL)
+    if (leer_usuarios(usuarios, cantidad) == 0)
     {
-        e = obtener_error(e1); 
-        while (e.codigo != 0)
-        {
-            printf("ERROR %d: %s\n", e.codigo, e.descripcion);
-            e = obtener_error(e1); 
-        }
-    }
+        int status1 = crear_usuario(usuarios, cantidad, errores);
 
-    printf("\nUsuario 2:\n");
-    if (e2 != NULL)
-    {
-        e = obtener_error(e2); 
-        while (e.codigo != 0)
-        {
-            printf("ERROR %d: %s\n", e.codigo, e.descripcion);
-            e = obtener_error(e2); 
-        }
-    }
+    /*
+    faC12Bar
+    NX
+    123ab
+    NX
+    ADMIN
+    OK
+    Fac123barerr
+    NX
+    1234.aB
+    NX
+    PROF
+    OK
+    lUC1aBarr
+    NX
+    aM3h2dI
+    NX
+    OK
 
-    printf("\nUsuario 3:\n");
-    if (e3 != NULL)
-    {
-        e = obtener_error(e3); 
-        while (e.codigo != 0)
-        {
-            printf("ERROR %d: %s\n", e.codigo, e.descripcion);
-            e = obtener_error(e3); 
-        }
+    */
+
+        printf("ESTADOS:\n\tstatus1=%d\n", status1);
+
+        printf("\nUsuario 1:\n");
+        if (errores != NULL)
+            mostrar_errores(errores);
     }
 }
 
