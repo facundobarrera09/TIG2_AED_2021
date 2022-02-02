d32a7baad0332f24aba4fbe0f13b7eb0e77494dc
Georgina Bordoy <bordoygeorgina7@gmail.com>
2022-02-02 10:37:43 -0300

    Codificar la funcion principal de CentroEstetica-asist.cpp

---
+++ CentroEstetica-asist.cpp
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "structs.h"
+#include "usuarios.h"
+
+void centro_estetica_asist(Error *&errores);
+void dar_alta();
+void registrar_turno();
+void obtener_informe();
+
+int main()
+{
+    Error *errores, error;
+
+    // Funcion principal
+    centro_estetica_asist(errores);
+
+    // Mostrar errores en pantalla
+    if (errores != NULL)
+    {
+        system("cls");
+        printf("\n\tERROR FATAL - El programa no puede continuar ejecutandose:\n");
+        error = obtener_error(errores);
+        while (error.codigo != 0)
+        {
+            printf("\t - %d: %s\n", error.codigo, error.descripcion);
+            error = obtener_error(errores);
+        }
+
+        return 1;
+    }
+
+    return 0;
+}
+
+// FUNCION PRINCIPAL
+void centro_estetica_asist(Error *&errores)
+{
+    /**
+     * Funcion principal del programa CentroEstetica-asist.cpp
+     * Se encarga de todo lo relacionado a esta sección del programa y retorna una pila de errores fatales
+     * de los cuales el programa no se puede recuperar por si solo.
+     * 
+     */
+
+    Menu menu;
+    Sesion sesion;
+    Fecha hoy = obtener_fecha_actual();
+
+    Usuario usuarios[MAX_USUARIOS];
+    Cliente clientes[MAX_CLIENTES];
+    int cant_usuarios, cant_clientes;
+
+    int contador = 0;
+
+    errores = NULL;
+    Error *errores_creacion = NULL;
+    bool ejecutar = true;
+    int estado, opcion;
+
+    // Verificar si existen usuarios
+    while (true)
+    {
+        // Leer usuarios de usuarios.dat
+        estado = leer_usuarios(usuarios, cant_usuarios);
+
+        if (estado != 0)
+        {
+            // Si usuarios.dat esta vacio, terminar programa
+            if (estado == 1)
+                insertar_error(errores, C_INICIO_NO_USUARIOS);
+            else
+                insertar_error(errores, C_INICIO_NO_ARCHIVO);
+
+            return;
+        }
+        else
+            break;
+    }
+
+    // Iniciar sesion
+    while (true)
+    {
+        estado = inicio_de_sesion(sesion.usuario, COD_ADMIN, usuarios, cant_usuarios, errores);
+
+        if (estado != 0)
+        {
+            printf("\nError durante el inicio de sesion:\n");
+            mostrar_errores(errores);
+            printf("\n");
+            system("pause");
+        }
+        else
+            break;
+    }
+
+    // Mostrar menu
+    modificar_dato(menu, "largo", "11");
+    modificar_dato(menu, "ancho", "65");
+    modificar_dato(menu, "margen", "4");
+
+    modificar_dato(menu, "titulo", "Centro de Estetica");
+    modificar_dato(menu, "titulo", "Recepcion");
+
+    modificar_dato(menu, "opcion", "1-1 - Dar de alta a un cliente");
+    modificar_dato(menu, "opcion", "2-2 - Registrar un turno");
+    modificar_dato(menu, "opcion", "3-3 - Obtener informe");
+    modificar_dato(menu, "opcion", "4-");
+    modificar_dato(menu, "opcion", "5-0 - Salir");
+
+    modificar_dato(menu, "seleccion", "0");
+
+    modificar_dato(menu, "control", "Seleccione un numero");
+
+    while (ejecutar)
+    {
+        mostrar_menu(menu);
+        printf("\n\n> ");
+        scanf("%d", &opcion);
+
+        printf("\n");
+
+        switch (opcion)
+        {
+        case 1: // Dar de alta un cliente
+            dar_alta();
+            break;
+
+        case 2: // Registrar un turno
+            registrar_turno();
+            break;
+
+        case 3: // Obtener informe
+            obtener_informe();
+            break;
+
+        case 0:
+            printf("Programa terminado\n");
+            ejecutar = false;
+            break;
+        
+        default:
+            break;
+        }
+
+        printf("\n");
+        system("pause");
+    }
+}
+
+void dar_alta()
+{
+    
+}
+
+void registrar_turno()
+{
+
+}
+
+void obtener_informe()
+{
+
+}
