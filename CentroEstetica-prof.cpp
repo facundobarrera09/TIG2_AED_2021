2e060c9f4c584e6126f7c7d1bcfc5e3f500214e4
Pedro Cano Curtti <pedrocanocurtti@hotmail.com>
2022-02-08 22:01:47 -0300

    codificar opcion llamar_paciente()

--- CentroEstetica-prof.cpp
+++ CentroEstetica-prof.cpp
@@ -1,278 +1,29 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 #include <string.h>
-#include <iostream>
+
 #include "structs.h"
 #include "menu.h"
 #include "error.h"
 #include "usuarios.h"
 
-typedef struct {
-    char nombre[40];
-    char direccion[40];
-    int doc;
-    char historia[100];
-} tpaciente;
+void llamar_paciente();
+void buscar_historial();
 
-
-void continuar()
-{
-    printf("Presione una tecla para continuar\n\n");
-    getch();
-}
-
-void crear()
-{
-    FILE *arch;
-    arch=fopen(CLIENTES_DAT,"wb");
-    if (arch==NULL)
-        exit(1);
-    fclose(arch);
-    continuar();
-}
-
-
-void listado()
-{
-	tpaciente paciente,v[100],aux;
-	FILE *arch;
-	int i,n,b;
-	
-	arch=fopen(CLIENTES_DAT,"rb");
-        if (arch==NULL)
-        exit(1);
-   
-    fread(&paciente, sizeof(tpaciente), 1, arch);
-    while (feof(arch)==0)//paso a un vector el archivo
-    {
-    	v[i]=paciente;
-    	i++;
-    	fread(&paciente, sizeof(tpaciente), 1, arch);
-    }
-    n=i;
-    
-	//ordeno alfabeticament el vecctor
-		 
-        do
-        {
-        	b=0;
-        	for(i=0;i<n-1;i++)
-        	{
-        		if(strcmp(v[i].nombre,v[i+1].nombre)>0)
-        		{
-        			aux=v[i];
-        			v[i]=v[i+1];
-        			v[i+1]=aux;
-        			b=1;
-        		}
-        	}
-        }
-    	while (b==1);
-    	
-    rewind(arch);
-    for(i=0; i<n; i++)
-    {
-    	paciente=v[i];
-    	fwrite(&paciente,sizeof(tpaciente),1,arch);
-    }
-    
-   // getch ();
-    
-    rewind(arch);
-   fread(&paciente, sizeof(tpaciente), 1, arch);
-   	printf("\nLista de pacientes\n");
-	printf("\nApellido/nom\t\tDocumento\tDireccion\n");
-	printf("**********************************************************\n");
-	while(!feof(arch))
-    {
-        printf("%s\t\t%d\t\t%s\n%s\n\n", paciente.nombre, paciente.doc, paciente.direccion,paciente.historia);
-        fread(&paciente, sizeof(tpaciente), 1, arch);
-    }
-    fclose(arch);
-    continuar();
-}
-
-void consulta()
-{
-	char histclinic [1000];
-	int ag;
-    FILE *arch;
-    arch=fopen(CLIENTES_DAT,"r+b");
-    if (arch==NULL)
-        exit(1);
-    printf("Ingrese el documento a consultar:");
-    int docu;
-    scanf("%i", &docu);
-    tpaciente paciente;
-    int existe=0;
-    fread(&paciente, sizeof(tpaciente), 1, arch);
-    while(!feof(arch))
-    {
-        if (docu==paciente.doc)
-        {
-           printf("%s, %s, %d\n%s", paciente.nombre, paciente.direccion, paciente.doc,paciente.historia);
-           existe=1;
-           break;
-        }
-        fread(&paciente, sizeof(tpaciente), 1, arch);
-    }
-    if (existe==0)
-        printf("No existe dicho doc\n");
-        printf("\n\n\n");
-        printf("Desea agregar historia clinica? : 1 <--para si  /  0<--para No.\n");
-        scanf("%d", &ag);
-        
-        if(ag==1)
-        {
-        	fflush(stdin);
-           printf("Ingrese nueva historia clinica:");
-           gets(histclinic);
-           strcat(paciente.historia, "\n" );
- 			strcat(paciente.historia, histclinic );
-           //scanf("%f",&paciente.direccion);
-           int pos=ftell(arch)-sizeof(tpaciente);
-           fseek(arch,pos,SEEK_SET);
-           fwrite(&paciente, sizeof(tpaciente), 1, arch);
-           printf("\nSe agrego historia clinica.\n\n");
-        }
-        fread(&paciente, sizeof(tpaciente), 1, arch);
-    fclose(arch);
-    continuar();
-}
-
-
-
-void modificacionInforme()
-{
-	char histclinic [1000];
-    FILE *arch;
-    arch=fopen(CLIENTES_DAT,"r+b");
-    if (arch==NULL)
-        exit(1);
-    printf("Ingrese el documento a modificar:");
-    int docu;
-    scanf("%d", &docu);
-    tpaciente paciente;
-    int existe=0;
-    fread(&paciente, sizeof(tpaciente), 1, arch);
-    while(!feof(arch))
-    {
-        if (docu==paciente.doc)
-        {
-           printf("%i %s %0.2f\n", paciente.nombre, paciente.direccion, paciente.doc);
-           fflush(stdin);
-           printf("Ingrese nueva historia clinica:");
-           gets(histclinic);
-           strcat(paciente.historia, "\n" );
- 			strcat(paciente.historia, histclinic );
-           //scanf("%f",&paciente.direccion);
-           int pos=ftell(arch)-sizeof(tpaciente);
-           fseek(arch,pos,SEEK_SET);
-           fwrite(&paciente, sizeof(tpaciente), 1, arch);
-           printf("Se agrego historia clinica.\n");
-           existe=1;
-           break;
-        }
-        fread(&paciente, sizeof(tpaciente), 1, arch);
-    }
-    if (existe==0)
-        printf("No existe el documento\n");
-    fclose(arch);
-    continuar();
-}
-
-void modificacionDireccion()
-{
-    FILE *arch;
-    
-    arch=fopen(CLIENTES_DAT,"r+b");
-    
-    if (arch==NULL)
-        exit(1);
-    printf("Ingrese el documento a modificar:");
-    int docu;
-    scanf("%d", &docu);
-    tpaciente paciente;
-    int existe=0;
-    fread(&paciente, sizeof(tpaciente), 1, arch);
-    while(!feof(arch))
-    {
-        if (docu==paciente.doc)
-        {
-           printf("%i %s %0.2f\n", paciente.nombre, paciente.direccion, paciente.doc);
-           printf("Ingrese nuevo direccion:");
-           scanf("%f",&paciente.direccion);
-           int pos=ftell(arch)-sizeof(tpaciente);
-           fseek(arch,pos,SEEK_SET);
-           fwrite(&paciente, sizeof(tpaciente), 1, arch);
-           printf("Se modifico la direccion.\n");
-           existe=1;
-           break;
-        }
-        fread(&paciente, sizeof(tpaciente), 1, arch);
-    }
-    if (existe==0)
-        printf("No existe el documento\n");
-    fclose(arch);
-    continuar();
-}
-
-void borrapaciente()
-{
-
-	bool band;
-	FILE *arch;
-	FILE *aux;// archivo para guardar datos sin el paciente a borrar
-    
-	arch=fopen(CLIENTES_DAT,"r+b");
-    aux=fopen("auxiliar.dat","w+b");
-   // if (arch==NULL)
-     //   exit(1);
-        
-    printf("Ingrese el documento a borrar:");
-    int docu;
-    scanf("%d", &docu);
-    tpaciente paciente;
-    int existe=0;
-    fread(&paciente, sizeof(tpaciente),1,arch);
-    band=false;
-    while(!feof(arch))
-	{
-		if(paciente.doc!=docu)
-		
-		fwrite(&paciente,sizeof(tpaciente),1,aux);
-		
-		else
-		band=true;
-		
-		fread(&paciente,sizeof(tpaciente),1,arch);
-	}
-	fclose (arch);
-	fclose (aux);
-	
-	if(band==true)
-	{
-		printf("\nPaciente borrado.\n\n");
-		remove("pacientes.dat");
-		rename("auxiliar.dat","pacientes.dat");
-	}
-	else
-	printf("\nPaciente no encontrado\n\n");
-}
-
+Sesion sesion;
+Fecha hoy;
 
 int main()
 {
-	
     Menu menu;
-	Sesion sesion;
+    Profesional profesionales[MAX_PROF];
 	Usuario usuarios[MAX_USUARIOS];
-	int cantidad;
+	int cant_usuarios, cant_prof;
 	int estado;
 	Error *error_inicio;
+    hoy = obtener_fecha_actual();
 	
-	estado=leer_usuarios(usuarios,cantidad);
+	estado=leer_usuarios(usuarios,cant_usuarios);
 	
 	if(estado!=0)
 	{
@@ -283,65 +34,175 @@ int main()
 	{
 		while(true)
 		{
-			estado=inicio_de_sesion(sesion.usuario,COD_PROF,usuarios,cantidad,error_inicio);
+			estado = inicio_de_sesion(sesion.usuario,COD_PROF,usuarios,cant_usuarios,error_inicio);
 			if (estado!=0)
 			{
 				mostrar_errores(error_inicio);
-			
 				continue;
-				
 			}
 			else
-			break;
+            {
+                estado = leer_profesionales(profesionales, cant_prof);
+                if (estado == 0)
+                {
+                    estado = buscar_profesional(sesion.datos_prof, profesionales, cant_prof, sesion.usuario.usuario);
+                    if (estado != 0)
+                    {
+                        printf("No se encontro un profesional asociado al usuario");
+                        return 1;
+                    }
+                }
+                else
+                {
+                    printf("No se pudo leer archivo de profesionales");
+                    return 1;
+                }
+                
+			    break;
+            }
 		}
 	}
 	
 	int opcion;
 
-    modificar_dato(menu, "largo", "14");
-    modificar_dato(menu, "ancho", "71");
-    modificar_dato(menu, "margen", "2");
+    modificar_dato(menu, "largo", "10");
+    modificar_dato(menu, "ancho", "65");
+    modificar_dato(menu, "margen", "4");
 
     // Titulo
     modificar_dato(menu, "titulo", "Centro de Estetica");
-    modificar_dato(menu, "titulo", "Menu de Profesionales");
+    modificar_dato(menu, "titulo", "Profesionales");
 
     // Opciones y valores
-    modificar_dato(menu, "opcion", "0-1-Crear archivo");
-    modificar_dato(menu, "opcion", "1-2-Cargar cliente");
-    modificar_dato(menu, "opcion", "2-3-listar clientes");
-    modificar_dato(menu, "opcion", "3-4-Consultar cliente por DNI");
-    modificar_dato(menu, "opcion", "4-5-Modificar direccion");
-    modificar_dato(menu, "opcion", "5-6-Agregar informe");
-    modificar_dato(menu, "opcion", "6-7-borrar cliente");
-    modificar_dato(menu, "opcion", "7- ");
-    modificar_dato(menu, "opcion", "8-8-Salir");
+    modificar_dato(menu, "opcion", "0-1 - Llamar al siguiente paciente");
+    modificar_dato(menu, "opcion", "1-2 - Obtener historial de un paciente");
+    modificar_dato(menu, "opcion", "2-");
+    modificar_dato(menu, "opcion", "3-0 - Salir");
+
+    // Controles
+    modificar_dato(menu, "control", "Seleccione una opcion");
     
     do {
-    mostrar_menu(menu);
-
-        scanf("%d",&opcion);
+        mostrar_menu(menu);
+        printf("\n> ");
+        scanf("%d", &opcion);
 
         switch (opcion) {
             case 1:
-                    crear();
-                   break;
+                llamar_paciente();
+                break;
             case 2:
-                    escribir_cliente(crear_cliente());
-                   break;
-            case 3:listado();
-                   break;
-            case 4:consulta();
-                   break;
-            case 5:modificacionDireccion();
-                   break;
-            case 6:modificacionInforme();
-            		break;
-            case 7:borrapaciente();
-            		break;
-            
-          		//	break;
+                buscar_historial();
+                break;
         }
-    } while (opcion!=8);
+
+        printf("\n");
+        system("pause");
+
+    } while (opcion != 0);
+
     return 0;
+}
+
+void llamar_paciente()
+{
+    Menu datos_paciente;
+
+    Cliente turnos[MAX_TURNOS], paciente;
+    Informe informes[MAX_INFORMES], datos_informe;
+    int cant_turnos, cant_informes;
+
+    Error *errores;
+
+    int estado, opcion;
+    char buffer[100] = "";
+
+    // Menu de datos del paciente
+    modificar_dato(datos_paciente, "largo", "flex");
+    modificar_dato(datos_paciente, "largo", "10");
+    modificar_dato(datos_paciente, "ancho", "65");
+    modificar_dato(datos_paciente, "margen", "4");
+    modificar_dato(datos_paciente, "titulo", "Datos del paciente");
+    modificar_dato(datos_paciente, "opcion", "0-Nombre");
+    modificar_dato(datos_paciente, "opcion", "1-DNI");
+    modificar_dato(datos_paciente, "opcion", "2-Domicilio");
+    modificar_dato(datos_paciente, "opcion", "3-Edad");
+    modificar_dato(datos_paciente, "opcion", "4-Peso");
+    modificar_dato(datos_paciente, "control", "Enter para continuar");
+
+    // Mostrar datos en pantalla
+    estado = leer_turnos(turnos, cant_turnos);
+
+    if (estado == 0)
+    {
+        paciente = turnos[0];
+        //borrar_turno();
+
+        // Actualizar menu
+        strcpy(buffer, "0-");
+        strcat(buffer, paciente.nombre);
+        modificar_dato(datos_paciente, "valor", buffer);
+
+        strcpy(buffer, "1-");
+        itoa(paciente.dni, &buffer[2], 10);
+        modificar_dato(datos_paciente, "valor", buffer);
+
+        strcpy(buffer, "2-");
+        strcat(buffer, paciente.domicilio);
+        modificar_dato(datos_paciente, "valor", buffer);
+
+        strcpy(buffer, "3-");
+        itoa(obtener_edad(paciente.fecha_nacimiento, hoy), &buffer[2], 10);
+        modificar_dato(datos_paciente, "valor", buffer);
+
+        strcpy(buffer, "4-");
+        snprintf(&buffer[2], (sizeof buffer)-2, "%f", paciente.peso);
+        modificar_dato(datos_paciente, "valor", buffer);
+
+        mostrar_menu(datos_paciente);
+        system("pause");
+    
+        // Crear informe
+        while (true)
+        {
+            datos_informe.dni_cliente = paciente.dni;
+            datos_informe.id_profesional = sesion.datos_prof.id_profesional;
+            datos_informe.fecha = hoy;
+
+            estado = crear_informe(datos_informe, informes, cant_informes, errores);
+
+            if (estado != 0)
+            {
+                mostrar_errores(errores);
+                printf("\n\n");
+                printf("Desea intentar de nuevo? [s/n]: ");
+                _flushall();
+                opcion = getchar();
+
+                if (opcion == 's' || opcion == 'S')
+                    continue;
+                else
+                    break;
+            }
+            else
+            {
+                borrar_turno();
+                printf("\nInforme registrado con exito\n");
+                break;
+            }
+        }
+    }
+    else if (estado == 1)
+    {
+        printf("No hay pacientes en la fila de espera\n");
+    }
+    else 
+    {
+        printf("Error al leer el archivo de turnos\n");
+    }
+}
+
+void buscar_historial()
+{
+
 }
